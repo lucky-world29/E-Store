e8c0d4c98bdde81c624637be0dd7fcce55835b6b
Abinash Behera <abinashbehera9898@gmail.com>
2026-07-21 20:34:33 +0530

    .....

--- estore/src/Components/TopNav/index.js
+++ estore/src/Components/TopNav/index.js
@@ -53,11 +53,9 @@ const TopNav = () => {
     <div>
       <div className="header bg-dark">
         <div className="row top-nav-row">
-
           <div className="brand my-1">
             <h1>eStore</h1>
           </div>
-
           <div className="inp-container p-0 my-4 w-50 h-25 bg-white">
             <div className="dropdown m-0 p-0">
               <select className="select-btn w-100 p-0 m-0">
@@ -66,9 +64,7 @@ const TopNav = () => {
                 <option>Kids</option>
               </select>
             </div>
-
             <input className="form-control" placeholder="Search..." />
-
             <button>
               <i className="fa fa-search" />
             </button>
